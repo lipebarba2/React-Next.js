9da100022b6ddbbd07d00e5a33030b49fbc61352
lipebarba2 <felipecinemkt@gmail.com>
2024-09-10 04:46:05 -0300

    feature estudos

---
+++ src/app/page.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { Person } from "@/types/Person";
+import { TodoItem } from "@/types/TodoItem";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Home() {
+
+
+    const [list, setList] = useState<TodoItem[]>([
+        {label: 'Estudar React', checked: false},
+        {label: 'Estudar Node', checked: false}
+    ])
+
+
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center text-2xl">
+
+        <h1 className=" text-4xl mt-5">Lista de Tarefas</h1>
+
+        <div className="flex w-full max-w-lg my-3 p-4 rounded-md bg-gray-700 border-2">
+            <input
+                type="text"
+                placeholder="O que deseja fazer?"
+                className="flex-1 border border-black p-3 text2xl text-black rounded-b-md mr-3 " 
+                    
+                
+            />
+
+            <button >Adicionar</button>
+
+        </div>      
+
+        <ul className="w-full max-w-lg list-disc pl-5">
+
+            {list.map((item) => (
+                <li>
+                    
+                    {item.label}
+
+                    <button className="flex-1 p-3 bg-red-600 rounded-lg border-none m-6 hover:underline">Deletar</button>
+                
+                </li>
+            ))}
+            
+
+        </ul>
+
+    </div>
+  );
+}
