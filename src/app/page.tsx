74206b5223fad604b77159fc5ff2306bb6506bb2
lipebarba2 <felipecinemkt@gmail.com>
2024-09-10 06:22:39 -0300

    feat funcionalidade de excluir

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,27 @@ import { useState } from "react";
 
 export default function Home() {
 
-
+    const [itemInput, setItemInput] = useState('')
     const [list, setList] = useState<TodoItem[]>([
         {label: 'Estudar React', checked: false},
         {label: 'Estudar Node', checked: false}
     ])
 
+    const handleAddButton = () => {
+        if (itemInput.trim() !== '')
+        setList([
+            ...list,
+            {label: itemInput, checked: false}
+        ])
+
+        setItemInput('')
+    }
+
+    const handleDeleteButton = (index: number) => {
+        setList(list.filter((item, key) => (
+            key !== index
+        )))
+    }
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center text-2xl">
@@ -24,22 +39,29 @@ export default function Home() {
                 type="text"
                 placeholder="O que deseja fazer?"
                 className="flex-1 border border-black p-3 text2xl text-black rounded-b-md mr-3 " 
-                    
-                
+                value={itemInput}
+                onChange={e => setItemInput(e.target.value)}
             />
 
-            <button >Adicionar</button>
+            <button onClick={handleAddButton} >Adicionar</button>
 
-        </div>      
+        </div>    
+
+        <p className="my-4">{list.length} items na lista</p>  
 
         <ul className="w-full max-w-lg list-disc pl-5">
 
-            {list.map((item) => (
-                <li>
+            {list.map((item,index) => (
+                <li key={index}>
                     
                     {item.label}
 
-                    <button className="flex-1 p-3 bg-red-600 rounded-lg border-none m-6 hover:underline">Deletar</button>
+                    <button 
+                        className="flex-1 p-3 bg-red-600 rounded-lg border-none m-6 hover:underline"
+                        onClick={() => handleDeleteButton(index)}
+                        >
+                            Deletar
+                    </button>
                 
                 </li>
             ))}
